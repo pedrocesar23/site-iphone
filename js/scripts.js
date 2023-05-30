b1359c96db8f50d487590f5f63de8d1ce8d86228
pedrocesar23 <126528993+pedrocesar23@users.noreply.github.com>
2023-05-30 14:31:35 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    $("#banners ul").bxSlider({
+        auto: true,
+        // mode: 'horizontal',
+        // speed: 500,
+        // page: true,
+    });
+
+    $('#fotos .container').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        closeOnContentClick: false,
+        closeBtnInside: false,
+        mainClass: 'mfp-with-zoom mfp-img-mobile',
+        image: {
+            verticalFit: true,
+        },
+        gallery: {
+            enabled: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300, // don't foget to change the duration also in CSS
+            opener: function(element) {
+                return element.find('img');
+            }
+        }
+
+    });
+});
